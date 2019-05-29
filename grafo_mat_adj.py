376480e3b728107d14d0b7b678cb96eb625b88de
Weverton Leite Da Silva <weverton1995@hotmail.com>
2019-05-29 15:38:53 -0300

    checkpoint 2 - slides 5 7 9 10 11 e 13

--- grafo_mat_adj.py
+++ grafo_mat_adj.py
@@ -2,11 +2,14 @@ from grafo import Grafo
 
 class GrafoMatrizAdj(Grafo):
 
-    def definirN(self, n):
+    def definirN(self, n: int):
         super(GrafoMatrizAdj, self).definirN(n)
         self.M = self.createMatrizAdj()
+        self.explorado = []
+        self.visitado = []
+        self.descoberto = []
 
-    def addVertice(self, v1):
+    def addVertice(self, v1: int):
         self.V.append(v1)
         self.V.sort()
         self.n += 1
@@ -30,7 +33,7 @@ class GrafoMatrizAdj(Grafo):
         self.M[v2][v1] = 1
         self.m += 1
 
-    def removeAresta(self, v1, v2):
+    def removeAresta(self, v1: int, v2: int):
         self.M[v1][v2] = 0
         self.M[v2][v1] = 0
         self.m -= 1
@@ -38,7 +41,7 @@ class GrafoMatrizAdj(Grafo):
     def is_adj(self, v1, v2):
         return self.M[v1][v2] == 1
 
-    def getListVerticesAdj(self, v):
+    def getListVerticesAdj(self, v: int):
         lista = []
         for i in range(1, self.n + 1):
             if(self.is_adj(v, i)):
@@ -59,3 +62,76 @@ class GrafoMatrizAdj(Grafo):
         for i in range(1, self.n + 1):
             aux[i] = [0] * (self.n + 1)
         return aux
+
+    #  CheckPoint 2
+
+    def rotular(self):
+        self.visitado= [False] * self.n
+        for i in range(self.n):
+            self.explorado.append([False] * self.n)
+            self.descoberto.append([False]* self.n)
+    
+    # Slide 5
+    def busca(self):
+        self.rotular()           
+        self.buscaSlide5(3)
+
+    def buscaSlide5(self, r: int):
+        self.visitado[r] = True
+        for i in range(self.n):
+            for j in range(self.n):                    
+                if(self.visitado[i] and  not self.explorado[i][j]):
+                    self.explorado[i][j] = True
+                    if(not self.visitado[j]):
+                        self.visitado[j] = True
+                        self.descoberto[i][j] = True 
+
+    #Slide 6
+    def buscaCompleta(self):
+        self.rotular()
+        for i in range(self.n):
+            if( not self.visitado[i]):
+                self.buscaSlide5(i)
+
+    # Slide 9
+    def ehConexo(self) -> bool:
+        self.busca()
+        for i in range(self.n):
+            if(not self.visitado[i]):
+                return False
+            return True  
+
+    # Slide 10
+    def temCiclo(self) -> bool:
+        self.buscaCompleta()
+        for i in range(self.n):
+            for j in range(self.n):
+                if(self.m[i][j] == 1):
+                    if(not self.descoberto[i][j]):
+                        return True
+        return False
+
+    # Slide 11
+    def ehFloresta(self) -> bool:
+        return not self.temCiclo()
+
+    # Slide 12
+    def ehArvore(self) -> bool:
+        self.busca()
+        for i in range(self.n):
+            if(not self.visitado[i]):
+                return False
+            return True  
+
+    # Slide 13
+    def ehArvoreSlide13(self) -> bool:
+        return self.ehConexo and not self.temCiclo()
+
+    # Slide 17
+    # def obterFlorestaGeradora(self) -> GrafoMatrizAdj:
+    #     T = GrafoMatrizAdj()
+    #     T.n = self.n
+    #     T.m = self.m
+    #     self.buscaCompleta()
+
+    #     return T
